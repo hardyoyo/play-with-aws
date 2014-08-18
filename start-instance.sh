e6cad37600f49b121e5f83c91dce323d4d3da30a
Hardy Pottinger <pottingerhj@umsystem.edu>
2014-08-18 02:59:07 -0500

    added a wait and check for running instance to the end of start-instance.sh

--- start-instance.sh
+++ start-instance.sh
@@ -2,3 +2,9 @@
 
 # a super-quick and dirty wrapper for ec2-run-instances, so I don't have to remember all this stuff
 ec2-run-instances ami-aaff23c2 -k home-made -g sg-c4e8bda1 -f cloudinit.yaml --instance-type t1.micro
+
+echo "waiting 30 seconds..."
+
+sleep 30
+
+ec2-describe-instances | grep running
