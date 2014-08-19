1946211219433a55aa64b58c8964307ae1192b38
Hardy Pottinger <pottingerhj@umsystem.edu>
2014-08-19 17:21:41 -0500

    adding tomcat6 to the list of packages installed by cloudinit, made librarian puppet stop stomping on my templates, added authbind for binding tomcat6 to port 80, added a reminder to be patient to the end of start-instance.sh

--- start-instance.sh
+++ start-instance.sh
@@ -3,8 +3,11 @@
 # a super-quick and dirty wrapper for ec2-run-instances, so I don't have to remember all this stuff
 ec2-run-instances ami-aaff23c2 -k home-made -g sg-c4e8bda1 -f cloudinit.yaml --instance-type t1.micro
 
-echo "waiting 30 seconds..."
+echo "waiting 160 seconds (the typical cloudinit duration)..."
+echo "  REMINDER: you might want to "
+echo "              tail -f /var/log/cloud-init-output.log"
+echo "            just to be sure, after you login..."
 
-sleep 30
+sleep 160
 
 ec2-describe-instances | grep running
